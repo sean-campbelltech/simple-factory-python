a6641a34e885ba5fa924f9d9170bcf899843f638
sean <sean@campbelltech.co.za>
2023-08-15 18:09:26 +0200

    Use correct name

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 from payment_factory import PaymentFactory
 
 factory = PaymentFactory()
-payment = factory.create("CreditCardPayment1")
+payment = factory.create("CreditCardPayment")
 payment.pay(1000)
