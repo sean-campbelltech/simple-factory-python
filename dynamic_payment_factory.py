be59820889d2e004c7ca536de714bd99409a553c
sean <sean@campbelltech.co.za>
2023-08-22 17:41:38 +0200

    Changed dictionary name to prevent ambiguity

--- dynamic_payment_factory.py
+++ dynamic_payment_factory.py
@@ -5,7 +5,7 @@ import payment_methods
 # Creator
 class DynamicPaymentFactory(object):
     # A dictionary to store the available payment implementations
-    payment_implementations = {}
+    payment_dictionary = {}
 
     def __init__(self):
         self.load_payment_methods()
@@ -16,11 +16,11 @@ class DynamicPaymentFactory(object):
         )
         for name, _type in members:
             if isclass(_type) and issubclass(_type, payment_methods.Payment):
-                self.payment_implementations[name] = _type
+                self.payment_dictionary[name] = _type
 
     def create(self, payment_type: str):
-        if payment_type in self.payment_implementations:
-            return self.payment_implementations[payment_type]()
+        if payment_type in self.payment_dictionary:
+            return self.payment_dictionary[payment_type]()
         else:
             raise ValueError(
                 f"{payment_type} is not currently supported as a payment method."
